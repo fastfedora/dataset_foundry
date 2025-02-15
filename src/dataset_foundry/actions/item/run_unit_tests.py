ca4f055d4bbf038031418ba705c1cd2d058ac80c
Trevor Lohrbeer <trevor@fastfedora.com>
2025-02-15 13:14:50 +0000

    feat!: Push full result in `run_unit_tests`
    Push the full unit test result object in `run_unit_tests`.
    
    Because we’re no longer pushing individual properties, the `prefix` parameter has been renamed `property` and the default value has been changed.

--- src/dataset_foundry/actions/item/run_unit_tests.py
+++ src/dataset_foundry/actions/item/run_unit_tests.py
@@ -9,12 +9,12 @@ from ...utils.unit_tests.run_python_unit_tests import run_python_unit_tests
 def run_unit_tests(
         filename: Union[Callable,Key,str],
         dir: Union[Callable,Key,str] = Key("input_dir"),
-        prefix: Union[Callable,Key,str] = "test_",
+        property: Union[Callable,Key,str] = "test_result",
     ):
     async def run_unit_tests_action(item: DatasetItem, context: Context):
         resolved_filename = resolve_item_value(filename, item, context, required_as="filename")
         resolved_dir = resolve_item_value(dir, item, context, required_as="dir")
-        resolved_prefix = resolve_item_value(prefix, item, context, required_as="prefix")
+        resolved_property = resolve_item_value(property, item, context, required_as="property")
 
         if isinstance(resolved_filename, str):
             format_data = {**item.data, 'id': item.id}
@@ -22,10 +22,6 @@ def run_unit_tests(
 
         result = run_python_unit_tests(resolved_dir / resolved_filename)
 
-        item.push({
-            f"{resolved_prefix}returncode": result.returncode,
-            f"{resolved_prefix}stdout": result.stdout,
-            f"{resolved_prefix}stderr": result.stderr,
-        }, run_unit_tests)
+        item.push({ resolved_property: result }, run_unit_tests)
 
     return run_unit_tests_action
