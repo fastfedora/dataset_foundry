0b35885ae8436af8bb2ed2804e5d17a96346a2a4
Trevor Lohrbeer <trevor@fastfedora.com>
2025-02-15 13:07:52 +0000

    feat: Add `get` utility
    Retrieves nested dictionary items or object attibutes using a dotted path (e.g., `get(item, ‘test_result.stdout’)` returns the value of  `item[‘test_result’].stdout`.

---
+++ src/dataset_foundry/utils/get.py
@@ -0,0 +1,37 @@
+from typing import Any, Union, Sequence
+from functools import reduce
+
+def get(obj: Any, path: Union[str, Sequence[str]], default: Any = None) -> Any:
+    """
+    Access nested dictionary items or object attributes using a path string or sequence.
+
+    Args:
+        obj: The object or dictionary to traverse
+        path: Either a dot-separated string ('a.b.c') or sequence of keys/attrs ['a', 'b', 'c']
+        default: Value to return if path doesn't exist
+
+    Returns:
+        The value at the path or the default value if not found
+
+    Examples:
+        >>> data = {'a': {'b': {'c': 1}}}
+        >>> get_in(data, 'a.b.c')
+        1
+        >>> get_in(data, ['a', 'b', 'c'])
+        1
+
+        >>> class Obj: pass
+        >>> o = Obj()
+        >>> o.a = {'b': 3}
+        >>> get_in(o, 'a.b')
+        3
+    """
+    if isinstance(path, str):
+        path = path.split('.')
+
+    try:
+        return reduce(lambda d, key: getattr(d, key, None) if hasattr(d, key)
+                     else d.get(key) if hasattr(d, 'get')
+                     else None, path, obj)
+    except (AttributeError, KeyError, TypeError):
+        return default
