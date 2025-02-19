5e2a37616b5757b0b1286dd0eb0207ff2d967585
Trevor Lohrbeer <trevor@fastfedora.com>
2025-02-19 12:16:42 +0000

    feat: Add `message` as param to `log_item`
    Can be used to log a message about an item, rather than properties of an item. If `message` is passed in, `properties` is ignored. If both are passed in, an error is raised.
    
    Can be used in a pipeline to log an info message like:
    
    ```
    log_item(message="Regenerating unit tests for {id}...")
    ```

--- src/dataset_foundry/actions/item/log_item.py
+++ src/dataset_foundry/actions/item/log_item.py
@@ -1,28 +1,44 @@
-from typing import List, Optional
+from typing import List, Optional, Union, Callable
 from pprint import pformat
 import logging
 
 from ...core.context import Context
 from ...core.dataset_item import DatasetItem
+from ...core.key import Key
 from ...utils.get import get
+from ...utils.params.resolve_item_value import resolve_item_value
+from ...utils.format.format_template import format_template
 
 logger = logging.getLogger(__name__)
 
 def log_item(
-        properties: Optional[List[str]] = None
+        properties: Optional[List[str]] = None,
+        message: Optional[Union[Callable,Key,str]] = None,
     ):
-    async def log_item_action(item: DatasetItem, _context: Context):
-        if properties:
-            if len(properties) == 1:
-                message = f"[{item.id}] {properties[0]}: {get(item.data, properties[0])}"
-            else:
-                filtered_data = {
-                    key: get(item.data, key.split('.')) for key in properties
-                }
-                message = f"[{item.id}] {pformat(filtered_data)}"
-        else:
-            message = f"[{item.id}] {pformat(item.data)}"
+    async def log_item_action(item: DatasetItem, context: Context):
+        resolved_message = resolve_item_value(message, item, context)
 
-        logger.info(message)
+        if isinstance(resolved_message, str):
+            format_data = {**item.data, 'id': item.id}
+            resolved_message = format_template(resolved_message, format_data)
+
+        if (resolved_message):
+            if properties:
+                raise ValueError("'properties' and 'message' cannot both be provided")
+
+            logger.info(resolved_message)
+        else:
+            if properties:
+                if len(properties) == 1:
+                    output = f"[{item.id}] {properties[0]}: {get(item.data, properties[0])}"
+                else:
+                    filtered_data = {
+                        key: get(item.data, key.split('.')) for key in properties
+                    }
+                    output = f"[{item.id}] {pformat(filtered_data)}"
+            else:
+                output = f"[{item.id}] {pformat(item.data)}"
+
+            logger.info(output)
 
     return log_item_action
