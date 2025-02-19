dc67ac15457455953c888be2d3a576f6a82eaad5
Trevor Lohrbeer <trevor@fastfedora.com>
2025-02-19 12:23:52 +0000

    docs: Add pipeline to run unit tests across folders
    Useful when the samples are each in their own folder, instead of in a single folder.

---
+++ examples/refactorable_code/run_unit_tests_by_folder/pipeline.py
@@ -0,0 +1,18 @@
+from dataset_foundry.actions.dataset.load_dataset_from_directory import load_dataset_from_directory
+from dataset_foundry.actions.item.if_item import if_item
+from dataset_foundry.actions.item.log_item import log_item
+from dataset_foundry.actions.item.run_unit_tests import run_unit_tests
+from dataset_foundry.core.pipeline import Pipeline
+
+pipeline = Pipeline(
+    setup=[
+        load_dataset_from_directory(include="{id|[0-9]*}_{function_name}/info.yaml"),
+    ],
+    actions=[
+        run_unit_tests(filename="{id}_{function_name}/func_{id}_{function_name}_test.py"),
+        log_item(properties=['test_result']),
+        if_item("not item.data['test_result'].success", [
+            log_item(properties=['test_result.stdout']),
+        ])
+    ]
+)
