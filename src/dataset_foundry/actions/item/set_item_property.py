ba6077c38dee8bceb0581905c1a6d7868c166cb7
Trevor Lohrbeer <trevor@fastfedora.com>
2025-02-28 16:30:37 +0000

    feat: Add `set_item_property` item action

---
+++ src/dataset_foundry/actions/item/set_item_property.py
@@ -0,0 +1,21 @@
+from typing import Callable, Union
+
+from ...core.context import Context
+from ...core.dataset_item import DatasetItem
+from ...core.key import Key
+from ...types.item_action import ItemAction
+from ...utils.params.resolve_item_value import resolve_item_value
+
+def set_item_property(
+        key: Union[Callable,Key,str],
+        value: Union[Callable,Key,str],
+    ) -> ItemAction:
+    async def set_item_property_action(item: DatasetItem, context: Context):
+        resolved_key = resolve_item_value(key, item, context, required_as="key")
+        resolved_value = resolve_item_value(value, item, context, required_as="value")
+
+        item.push({
+            resolved_key: resolved_value
+        }, set_item_property);
+
+    return set_item_property_action
