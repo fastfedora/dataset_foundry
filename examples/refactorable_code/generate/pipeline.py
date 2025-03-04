a4c7c413acc5f4d53d0bb0bbd0bdea8b6286b408
Trevor Lohrbeer <trevor@fastfedora.com>
2025-03-04 21:03:18 +0000

    feat(examples): Update master `generate` pipeline
    - Convert it to an `ItemPipeline` that uses `do_item_steps` instead of `run_pipeline` to allow item actions to be used in steps
    - Add look to regenerate both code and unit tests if unit tests keep failing

--- examples/refactorable_code/generate/pipeline.py
+++ examples/refactorable_code/generate/pipeline.py
@@ -1,16 +1,27 @@
 from dataset_foundry.actions.dataset.reset_dataset import reset_dataset
 from dataset_foundry.actions.dataset.run_pipeline import run_pipeline
-from dataset_foundry.core.dataset_pipeline import DatasetPipeline
+from dataset_foundry.actions.dataset.load_dataset import load_dataset
+from dataset_foundry.actions.item.do_item_steps import do_item_steps
+from dataset_foundry.actions.item.set_item_property import set_item_property
+from dataset_foundry.actions.item.while_item import while_item
+from dataset_foundry.core.template import Template
+from dataset_foundry.core.item_pipeline import ItemPipeline
 
 root_module = "examples.refactorable_code"
 
-pipeline = DatasetPipeline(
+pipeline = ItemPipeline(
     name="generate_refactorable_code",
-    steps=[
+    setup=[
         run_pipeline(pipeline=f"{root_module}.generate_spec.pipeline"),
         reset_dataset(), # `generate_spec` creates one item per spec file, not one per sample
-        run_pipeline(pipeline=f"{root_module}.generate_all_from_spec.pipeline"),
-        reset_dataset(), # Next pipeline not setup yet to use previous pipeline's output
-        run_pipeline(pipeline=f"{root_module}.regenerate_unit_tests.pipeline"),
+        load_dataset(filename="specs.yaml", property="spec"),
     ],
+    steps=[
+        set_item_property(key="status", value="not-done"),
+        while_item("item.data['status'] != 'done' and iteration < 2", [
+            do_item_steps(pipeline=f"{root_module}.generate_all_from_spec.pipeline"),
+            set_item_property(key="function_name", value=Template("{spec.name}")),
+            do_item_steps(pipeline=f"{root_module}.regenerate_unit_tests.pipeline"),
+        ]),
+    ]
 )
