56cdae1f57f1fb0a7d6837af3925bb4ae869e578
Trevor Lohrbeer <trevor@fastfedora.com>
2025-03-06 14:31:06 +0000

    feat: Add `set_item_metadata` item action
    Use to add metadata information into each item.

---
+++ src/dataset_foundry/actions/item/set_item_metadata.py
@@ -0,0 +1,39 @@
+from datetime import datetime
+from typing import Callable, Union
+
+from ...core.context import Context
+from ...core.dataset_item import DatasetItem
+from ...core.key import Key
+from ...types.item_action import ItemAction
+from ...utils.params.resolve_item_value import resolve_item_value
+
+def get_pipeline_metadata(context: Context) -> dict:
+    parent_metadata = get_pipeline_metadata(context.parent) if context.parent else None
+
+    return {
+        "name": context.pipeline.name,
+        **context.pipeline.metadata,
+        **({ "parent": parent_metadata } if parent_metadata else {}),
+    }
+
+def set_item_metadata(
+        property: Union[Callable,Key,str] = "metadata",
+    ) -> ItemAction:
+    async def set_item_metadata_action(item: DatasetItem, context: Context):
+        resolved_property = resolve_item_value(property, item, context, required_as="property")
+
+        metadata = {
+            "pipeline": get_pipeline_metadata(context),
+            "model": {
+                "name": context.model.model,
+                "kwargs": context.model.model_kwargs,
+                "temperature": context.temperature,
+            },
+            "created_at": datetime.now().isoformat(),
+        }
+
+        item.push({
+            resolved_property: metadata
+        }, set_item_metadata);
+
+    return set_item_metadata_action
