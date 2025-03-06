713d6a25d3476f9fd568033ae5ae2d12a80abb0d
Trevor Lohrbeer <trevor@fastfedora.com>
2025-03-06 14:40:47 +0000

    feat: Add else support to `if_item` item action

--- src/dataset_foundry/actions/item/if_item.py
+++ src/dataset_foundry/actions/item/if_item.py
@@ -6,24 +6,29 @@ from ...utils.eval.item_eval import item_eval
 
 logger = logging.getLogger(__name__)
 
-def if_item(condition: str, actions: list) -> ItemAction:
+def if_item(condition: str, if_actions: list, else_actions: list = []) -> ItemAction:
     """
     Creates an action that executes a list of actions if the given condition is met.
 
     Args:
         condition (str): A string representing the condition to evaluate.
-        actions (list): A list of actions to execute if the condition is true.
+        if_actions (list): A list of actions to execute if the condition is true.
+        else_actions (list): A list of actions to execute if the condition is false.
 
     Returns:
-        function: A function that takes a Dataset and Context and executes the actions if the
-            condition is true.
+        function: A function that takes a Dataset and Context and executes the corresponding
+            actions based on the evaluated condition.
     """
     async def if_item_action(item: DatasetItem, context: Context):
         if item_eval(condition, item, context):
-            logger.debug(f"Condition '{condition}' met. Executing actions.")
-            for action in actions:
+            logger.debug(f"Condition '{condition}' met. Executing 'if' actions.")
+            for action in if_actions:
+                await action(item, context)
+        elif else_actions:
+            logger.debug(f"Condition '{condition}' not met. Executing 'else' actions.")
+            for action in else_actions:
                 await action(item, context)
         else:
-            logger.debug(f"Condition '{condition}' not met. Skipping actions.")
+            logger.debug(f"Condition '{condition}' not met. No actions to execute.")
 
     return if_item_action
