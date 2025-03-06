e8ab14460872bc43f844cb779b308a1761b87fdb
Trevor Lohrbeer <trevor@fastfedora.com>
2025-03-06 13:45:36 +0000

    feat: Add support for template dicts
    When using a `dict` as a template, the key or values that should be resolved as a template also need to be wrapped in `Template`. This prevents keys or values that you don’t want to be resolved from being resolved.
    
    For example:
    
    ```
    Template({
        "model": Template("{context.model}"),
        "temperature": Template("{context.temperature}"),
        "context": Template("{context}")
    })
    ```

--- src/dataset_foundry/core/template.py
+++ src/dataset_foundry/core/template.py
@@ -1,20 +1,28 @@
-from typing import Any, Dict, Optional
+from typing import Any, Dict, Optional, Union
 
 from ..utils.format.format_template import format_template
 
+TemplateValue = Union[str, Dict[Any, Any]]
+
 class Template:
     """
-    A string containing a template with variables within curly braces. Variables can be
-    referenced using dotted notation (e.g. `{item.data.name}`) and have custom formatters applied
-    (e.g. `{item.data|yaml}`).
+    An object that has strings, where each string may be a template with variables within curly
+    braces. Variables can be referenced using dotted notation (e.g. `{item.data.name}`) and have
+    custom formatters applied (e.g. `{item.data|yaml}`).
     """
-    _value: str
+    _value: TemplateValue
 
     @property
-    def value(self) -> str:
+    def value(self) -> TemplateValue:
         return self._value
 
-    def __init__(self, value: str):
+    def __init__(self, value: TemplateValue):
+        """
+        Initialize a new Template object.
+
+        Args:
+            value (TemplateValue): The value to be templated.
+        """
         self._value = value
 
     def resolve(
@@ -26,6 +34,10 @@ class Template:
         Replace variables in the template with their corresponding values, applying the custom
         formatters if provided.
 
+        If the template value is a string, then the string is resolved as a template. If the
+        template value is a dict, then any key or value in the dict that is a Template is resolved
+        as a template.
+
         Args:
             variables (dict): A dictionary of variables to replace in the template.
             formatters (Optional[dict]): A dictionary of formatters to apply to the variables. If
@@ -34,4 +46,20 @@ class Template:
         Returns:
             str: The template with variables replaced with their corresponding values.
         """
-        return format_template(self.value, variables, formatters)
+        if isinstance(self.value, str):
+            return format_template(self.value, variables, formatters)
+        elif isinstance(self.value, dict):
+            resolved_value = {}
+
+            for key, value in self.value.items():
+                if isinstance(key, Template):
+                    key = key.resolve(variables, formatters)
+
+                if isinstance(value, Template):
+                    value = value.resolve(variables, formatters)
+
+                resolved_value[key] = value
+
+            return resolved_value
+        else:
+            raise ValueError(f"Invalid type for template: {type(self.value)}")
