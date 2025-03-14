ef119e19efcc5ca0824c12c9d892e1fad751c2d0
Trevor Lohrbeer <trevor@fastfedora.com>
2025-03-14 21:35:49 +0000

    feat(examples): Add `add_refactor_task` pipeline
    Use to add specific tasks to existing samples.

---
+++ examples/refactorable_code/add_refactor_task/pipeline.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+
+from dataset_foundry.actions.dataset.load_dataset_from_directory import load_dataset_from_directory
+from dataset_foundry.actions.item.generate_item import generate_item
+from dataset_foundry.actions.item.log_item import log_item
+from dataset_foundry.actions.item.parse_item import parse_item
+from dataset_foundry.actions.item.save_item import save_item
+from dataset_foundry.core.item_pipeline import ItemPipeline
+from dataset_foundry.core.key import Key
+from dataset_foundry.core.template import Template
+from dataset_foundry.utils.collections.omit import omit
+
+pipeline = ItemPipeline(
+    name="add_refactor_task",
+    metadata={
+        "version": "0.1.0",
+        "author": "fastfedora",
+        "description": "Add tasks describing specific refactors to perform to each info.yaml file",
+    },
+    config=Path(__file__).parent / "config.yaml",
+    setup=[
+        load_dataset_from_directory(include="{id}/info.yaml"),
+        load_dataset_from_directory(include="{id}/source.py", property="code", merge=True),
+    ],
+    steps=[
+        log_item(message=Template("Generating refactor tasks for {id}...")),
+        generate_item(prompt=Key("context.prompts.generate_task")),
+        parse_item(code_block="yaml", output_key="refactor_tasks"),
+        save_item(
+            contents=(lambda item: {
+                'id': item.id,
+                **omit(['messages', 'response', 'output', 'code'], item.data),
+            }),
+            filename=Template("{id}/info.yaml"),
+            format="yaml"
+        ),
+    ]
+)
