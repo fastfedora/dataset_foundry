f09f893e5d1481c26cd157ba72cfbd6b60cd0bda
Trevor Lohrbeer <trevor@fastfedora.com>
2025-03-31 16:52:45 +0100

    feat: Add `else_actions` to `if_dataset`

--- src/dataset_foundry/actions/dataset/if_dataset.py
+++ src/dataset_foundry/actions/dataset/if_dataset.py
@@ -6,13 +6,14 @@ from ...utils.eval.dataset_eval import dataset_eval
 
 logger = logging.getLogger(__name__)
 
-def if_dataset(condition: str, actions: list) -> DatasetAction:
+def if_dataset(condition: str, if_actions: list, else_actions: list = None) -> DatasetAction:
     """
     Creates an action that executes a list of dataset actions if the given condition is met.
 
     Args:
         condition (str): A string representing the condition to evaluate.
-        actions (list): A list of dataset actions to execute if the condition is true.
+        if_actions (list): A list of dataset actions to execute if the condition is true.
+        else_actions (list): A list of dataset actions to execute if the condition is false.
 
     Returns:
         function: A function that takes a Dataset and Context and executes the actions if the
@@ -20,10 +21,14 @@ def if_dataset(condition: str, actions: list) -> DatasetAction:
     """
     async def if_dataset_action(dataset: Dataset, context: Context):
         if dataset_eval(condition, dataset, context):
-            logger.debug(f"Condition '{condition}' met. Executing actions.")
-            for action in actions:
+            logger.debug(f"Condition '{condition}' met. Executing 'if' actions.")
+            for action in if_actions:
+                await action(dataset, context)
+        elif else_actions:
+            logger.debug(f"Condition '{condition}' not met. Executing 'else' actions.")
+            for action in else_actions:
                 await action(dataset, context)
         else:
-            logger.debug(f"Condition '{condition}' not met. Skipping actions.")
+            logger.debug(f"Condition '{condition}' not met. No actions to execute.")
 
     return if_dataset_action
