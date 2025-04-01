f8ab11b99beeed45b5f4bd6b129a4ec8ac8ea1d2
Trevor Lohrbeer <trevor@fastfedora.com>
2025-04-01 14:58:27 +0100

    feat(examples): Avoid regenerating existing scenarios
    When generating scenarios across models, or across multiple runs, we want to make sure we’re not generating duplicate, or essentially duplicate, scenarios. So read in the prior scenarios into the dataset metadata, and then pass that to the model to have it avoid those scenarios.

--- examples/refactorable_code/generate_scenarios/pipeline.py
+++ examples/refactorable_code/generate_scenarios/pipeline.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 
 from dataset_foundry.actions.dataset.generate_dataset import generate_dataset
+from dataset_foundry.actions.dataset.if_dataset import if_dataset
+from dataset_foundry.actions.dataset.load_dataset_metadata import load_dataset_metadata
 from dataset_foundry.actions.item.log_item import log_item
 from dataset_foundry.actions.item.parse_item import parse_item
 from dataset_foundry.actions.item.save_item import save_item
@@ -17,7 +19,12 @@ pipeline = ItemPipeline(
     },
     config=Path(__file__).parent / "config.yaml",
     setup=[
-      generate_dataset(prompt=Key("context.prompts.generate_scenarios")),
+      if_dataset('path_exists(f"{context.output_dir}/prior-scenarios.yaml")', [
+          load_dataset_metadata(filename="prior-scenarios.yaml", property="prior_scenarios"),
+          generate_dataset(prompt=Key("context.prompts.generate_different_scenarios")),
+      ], [
+          generate_dataset(prompt=Key("context.prompts.generate_scenarios")),
+      ]),
     ],
     steps=[
       log_item(message=Template("Generating scenario for {id}...")),
