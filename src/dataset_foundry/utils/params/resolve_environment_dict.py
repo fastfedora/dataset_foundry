d07456daeb186b13df4f9211be9dfdf326303e4f
Trevor Lohrbeer <trevor@fastfedora.com>
2025-07-22 16:14:00 +0200

    feat: Add support for defaults to resolve env dict
    Allow defaults to be specified in the environment variable expressions using the Bash syntax (e.g., `${VARIABLE:-default}`).

--- src/dataset_foundry/utils/params/resolve_environment_dict.py
+++ src/dataset_foundry/utils/params/resolve_environment_dict.py
@@ -9,13 +9,15 @@ logger = logging.getLogger(__name__)
 def resolve_environment_dict(environment: Dict[str, Any]) -> Dict[str, Any]:
     """
     Resolve variables within an environment dict from the system environment. Variables should be
-    in the form of ${VAR_NAME}.
+    in the form of `${VAR_NAME}`. Default values can be provided by using the format
+    `${VAR_NAME:-default_value}`.
 
-    For example, here `LOGIN` will be resolved to the value of the `USER` environment variable:
+    For example, here `LOGIN` will be resolved to the value of the `USER` environment variable, or
+    `root` if the `USER` environment variable is not set:
 
     ```
     environment = resolve_environment_dict({
-        "LOGIN": "${USER}"
+        "LOGIN": "${USER:-root}"
     })
     ```
 
@@ -33,14 +35,21 @@ def resolve_environment_dict(environment: Dict[str, Any]) -> Dict[str, Any]:
 
     for key, value in environment.items():
         if isinstance(value, str) and value.startswith("${") and value.endswith("}"):
-            # Extract variable name and get from system environment
-            var_name = value[2:-1]  # Remove ${ and }
+            inner = value[2:-1]  # Remove ${ and }
+            if ':-' in inner:
+                var_name, default_value = inner.split(':-', 1)
+            else:
+                var_name, default_value = inner, None
             system_value = os.environ.get(var_name)
 
-            if system_value:
+            if system_value is not None and system_value != "":
+                print("...resolved", key, system_value)
                 environment[key] = system_value
+            elif default_value is not None and default_value != "":
+                print("...resolved", key, default_value)
+                environment[key] = default_value
             else:
                 # Pass through the expression as-is
-                logger.warning(f"Environment variable {var_name} not found")
+                logger.warning(f"Environment variable {var_name} not found and no default provided")
 
     return environment
