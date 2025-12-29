05ab244e4535ce6b05d8723f665c45082c1d1a40
Trevor Lohrbeer <trevor@fastfedora.com>
2025-12-29 21:44:51 +0100

    feat: Add `timeout` parameter to `exec_item` action

--- src/dataset_foundry/actions/item/exec_item.py
+++ src/dataset_foundry/actions/item/exec_item.py
@@ -16,6 +16,7 @@ def exec_item(
         command: Union[Callable, Key, str],
         cwd: Optional[Union[Callable, Key, str]] = Key("context.output_dir"),
         output_key: Union[Callable, Key, str] = "exec_result",
+        timeout: Union[Callable,Key,int] = 10,
     ) -> ItemAction:
     """
     Runs a shell command in the specified working directory and stores the result in the item.
@@ -24,11 +25,13 @@ def exec_item(
         command: The shell command to run (string or callable/key).
         cwd: The working directory to run the command in (string or callable/key).
         output_key: The key to store the result under in the item (default: 'exec_result').
+        timeout: The timeout for the command in seconds (default: 10).
     """
     async def exec_item_action(item: DatasetItem, context: Context):
         resolved_command = resolve_item_value(command, item, context, required_as="command")
         resolved_cwd = resolve_item_value(cwd, item, context, required_as="cwd")
         resolved_output_key = resolve_item_value(output_key, item, context)
+        resolved_timeout = resolve_item_value(timeout, item, context)
 
         if isinstance(resolved_cwd, str):
             resolved_cwd = Path(resolved_cwd)
@@ -43,6 +46,7 @@ def exec_item(
                     cwd=resolved_cwd,
                     capture_output=True,
                     text=True,
+                    timeout=resolved_timeout,
                 )
                 return {
                     "command": resolved_command,
